b7c3edd9894bdb07fe5b4c8c0161baa0b14bbfeb
Cameron St. Amant <cameronstamant@protonmail.com>
2020-10-05 22:37:44 -0600

    working on getting boxes to be equal height and width

---
+++ script.js
@@ -0,0 +1,45 @@
+let btn = document.createElement('button');
+btn.style.margin = '10px 10px 10px 10px';
+btn.innerHTML = 'Reset';
+btn.id ='button';
+
+gridSize = 4;
+
+document.body.appendChild(btn);
+let getBtn = document.getElementById('button');
+getBtn.addEventListener('click', () => {
+  gridSize = parseInt(prompt('Grid size?'));
+  gridSize *= gridSize;
+  clearGrid();
+  grid();
+});
+
+const getDiv = document.body;
+const div = document.createElement('div');
+div.id = 'container';
+div.className = 'flex-container';
+
+getDiv.appendChild(div);
+
+const getInnerDiv = document.getElementById('container');
+let a = 0;
+
+function grid () {
+for (let i = 0; i < gridSize; i++) {
+  let i = document.createElement('div');
+  a++;
+  i.id = 'a';
+  getInnerDiv.appendChild(i);
+}
+}
+
+grid();
+
+getInnerDiv.addEventListener('mouseover', function(e) {
+  e.target.style.background = 'brown';
+})
+
+function clearGrid () {
+  getInnerDiv.innerHTML = '';
+  getInnerDiv.background = 'white';
+}
